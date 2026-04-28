85b96cbf23bb13464d27857717c8a56c86f665e9
Joryer Jimenez <joryerjesus10@gmail.com>
2026-04-28 08:57:09 -0600

    feat: add employee/selectors.py with org-scoped and global query functions

---
+++ employee/selectors.py
@@ -0,0 +1,17 @@
+from django.db.models import QuerySet
+
+from organization.models import Organization
+
+from .models import Employee
+
+
+def get_employee_by_chat_id(*, chat_id: str) -> Employee:
+    return Employee.objects.get(chat_id=chat_id)
+
+
+def get_employee_by_phone(*, phone_number: str) -> Employee:
+    return Employee.objects.get(phone_number=phone_number)
+
+
+def get_employees_for_org(*, org: Organization) -> QuerySet:
+    return Employee.objects.filter(organization=org, is_active=True)
