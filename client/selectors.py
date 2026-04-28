5f756fad0fa110128496354e6ab5fac983ca9cc2
Joryer Jimenez <joryerjesus10@gmail.com>
2026-04-28 11:22:51 -0600

    feat: refactor DRF viewsets for phase 7

--- client/selectors.py
+++ client/selectors.py
@@ -9,5 +9,9 @@ def get_clients_for_org(*, org: Organization) -> QuerySet:
     return Client.objects.filter(organization=org, is_active=True)
 
 
+def get_client_by_pk(*, client_id: int) -> Client:
+    return Client.objects.get(pk=client_id)
+
+
 def get_client_by_id(*, org: Organization, client_id: int) -> Client:
     return Client.objects.get(organization=org, pk=client_id)
