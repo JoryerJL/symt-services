bc279da1fb68917f02cd868da9ceee43c6e48896
Joryer Jimenez <joryerjesus10@gmail.com>
2026-04-28 17:07:42 -0600

    feat: add super-admin organization panel

--- common/views.py
+++ common/views.py
@@ -13,3 +13,12 @@ class AdminRequiredMixin(AccessMixin):
         except Exception:
             raise PermissionDenied("Tu usuario no tiene una organización asignada.")
         return super().dispatch(request, *args, **kwargs)
+
+
+class SuperAdminRequiredMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        if not request.user.is_superuser:
+            raise PermissionDenied("Solo el super-admin puede acceder.")
+        return super().dispatch(request, *args, **kwargs)
