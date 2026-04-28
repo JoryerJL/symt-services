e6a933a9b371e36188142e3f88331af49e4f9c32
Joryer Jimenez <joryerjesus10@gmail.com>
2026-04-28 08:59:47 -0600

    feat: update service/views.py to use selectors and services

--- service/urls.py
+++ service/urls.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import ServiceListView, ServiceCreateView, AssignEmployeeView, reactivate_service, ServiceDetailView, generate_report_view, ServiceSummaryUpdateView
+from .views import (
+    AssignEmployeeView,
+    GenerateReportView,
+    ReactivateServiceView,
+    ServiceCreateView,
+    ServiceDetailView,
+    ServiceListView,
+    ServiceSummaryUpdateView,
+)
 
 urlpatterns = [
     path('', login_required(ServiceListView.as_view()), name='services_list'),
     path('create/', login_required(ServiceCreateView.as_view()), name='services_create'),
     path('assign-employee/', login_required(AssignEmployeeView.as_view()), name='assign_employee'),
-    path('reactivate-service/<int:pk>/', login_required(reactivate_service), name='reactivate_service'),
+    path('reactivate-service/<int:pk>/', login_required(ReactivateServiceView.as_view()), name='reactivate_service'),
     path('<int:pk>/', login_required(ServiceDetailView.as_view()), name='service_detail'),
-    path('<int:pk>/generate-report/', login_required(generate_report_view), name='generate_report'),
+    path('<int:pk>/generate-report/', login_required(GenerateReportView.as_view()), name='generate_report'),
     path('update-summary/', login_required(ServiceSummaryUpdateView.as_view()), name='update_service_summary'),
 ]
